4c227445b496aa1abc51be5ffa964851a9303fb6
Руслан <idrisovruslan074@gmail.com>
2020-10-05 22:11:15 +0300

    remove useless import
    Signed-off-by: Руслан <idrisovruslan074@gmail.com>

--- src/main/java/com/telefeed/TelefeedApplication.java
+++ src/main/java/com/telefeed/TelefeedApplication.java
@@ -2,9 +2,6 @@ package com.telefeed;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.telegram.telegrambots.ApiContextInitializer;
-
-import java.util.ArrayList;
 
 @SpringBootApplication
 public class TelefeedApplication {
